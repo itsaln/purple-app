80994ecd4ef275b0a9f1488ea1764e50fb7b7645
Aladin Mukhammadiev <itsaln23@gmail.com>
2024-06-28 16:17:37 +0500

    FlatList

--- app/(app)/index.tsx
+++ app/(app)/index.tsx
@@ -1,5 +1,11 @@
 import { useEffect } from 'react'
-import { ScrollView, StyleSheet, View } from 'react-native'
+import {
+	ActivityIndicator,
+	FlatList,
+	RefreshControl,
+	StyleSheet,
+	View
+} from 'react-native'
 import { useAtomValue, useSetAtom } from 'jotai'
 
 import {
@@ -7,31 +13,57 @@ import {
 	loadCourseAtom
 } from '@/entities/course/model/course.state'
 import { CourseCard } from '@/entities/course/ui/CourseCard/CourseCard'
-
-import { Gaps } from '@/shared/tokens'
+import { StudentCourseDescription } from '@/entities/course/model/course.model'
+import { Colors } from '@/shared/tokens'
 
 export default function MyCourses() {
 	const { isLoading, error, courses } = useAtomValue(courseAtom)
 	const loadCourse = useSetAtom(loadCourseAtom)
+
+	const renderCourse = ({ item }: { item: StudentCourseDescription }) => (
+		<View style={styles.item}>
+			<CourseCard {...item} />
+		</View>
+	)
 
 	useEffect(() => {
 		let ignore = loadCourse()
 	}, [])
 
 	return (
-		<ScrollView>
-			<View style={styles.wrapper}>
-				{courses.length > 0 &&
-					courses.map((c, i) => <CourseCard key={`${c.id}_${i}`} {...c} />)}
-			</View>
-		</ScrollView>
+		// <ScrollView>
+		// 	<View style={styles.wrapper}>
+		// 		{courses.length > 0 &&
+		// 			courses.map((c, i) => <CourseCard key={`${c.id}_${i}`} {...c} />)}
+		// 	</View>
+		// </ScrollView>
+
+		<>
+			{isLoading && <ActivityIndicator style={styles.activity} size='large' color={Colors.primary} />}
+			{courses.length > 0 && (
+				<FlatList
+					refreshControl={
+						<RefreshControl
+							refreshing={isLoading}
+							onRefresh={loadCourse}
+							tintColor={Colors.primary}
+							titleColor={Colors.primary}
+						/>
+					}
+					data={courses}
+					keyExtractor={(item, index) => `${item.id}_${index}`}
+					renderItem={renderCourse}
+				/>
+			)}
+		</>
 	)
 }
 
 const styles = StyleSheet.create({
-	wrapper: {
-		flexDirection: 'column',
-		gap: Gaps.g20,
+	activity: {
+		marginTop: 30
+	},
+	item: {
 		padding: 20
 	}
 })
